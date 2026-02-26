d6aa94b5d9b3cbb87360d83d05a64154a4cc6cf2
Karam-Kreidli <karam.kreidli@gmail.com>
2026-02-26 13:52:55 +0400

    chore: stop tracking supabase folder

--- supabase/add_declined_seen_by.sql
+++
@@ -1,7 +0,0 @@
--- Add declined_seen_by column to matches table
--- This stores an array of user IDs who have seen the decline notification
-
-ALTER TABLE matches ADD COLUMN IF NOT EXISTS declined_seen_by UUID[] DEFAULT '{}';
-
--- Create an index for efficient lookups
-CREATE INDEX IF NOT EXISTS idx_matches_declined_seen_by ON matches USING GIN(declined_seen_by);
